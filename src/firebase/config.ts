08856fbb330bf5c2d628b27ce94a69cbe48403bd
fekadu Hora <fekaduhora28@gmail.com>
2026-02-28 12:22:25 +0000

    what happ

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 
 export const firebaseConfig = {
-  apiKey: "placeholder-api-key",
-  authDomain: "placeholder-auth-domain",
-  projectId: "placeholder-project-id",
-  storageBucket: "placeholder-storage-bucket",
-  messagingSenderId: "placeholder-messaging-sender-id",
-  appId: "placeholder-app-id"
+  apiKey: "AIzaSyAfJmLx2mXs8dZ-N_P4RFtSYKnC5ZTRkn4",
+  authDomain: "mcf-rh.firebaseapp.com",
+  projectId: "mcf-rh",
+  storageBucket: "mcf-rh.firebasestorage.app",
+  messagingSenderId: "67245105848",
+  appId: "1:67245105848:web:d05e4c103e6a0c98eb4a4d"
 };
